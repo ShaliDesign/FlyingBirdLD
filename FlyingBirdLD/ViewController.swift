b4e0aa0edf527e611c9f9c4d5ed5fda258a0661e
Linh&ShawnD <linhshawn-d@linhshawnds-macbook-pro.local>
2017-04-11 15:25:47 +0700

    Hoan Thanh Tutorial

--- FlyingBirdLD/ViewController.swift
+++ FlyingBirdLD/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+
     
     var bird = UIImageView()
     
-    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +23,7 @@ class ViewController: UIViewController {
         drawJungle()
         addBird()
         flyUpAndDown()
-//        playSong()
+        playSong()
         
         
     }
@@ -46,52 +49,40 @@ class ViewController: UIViewController {
   func flyUpAndDown()
   {
     UIView.animate(withDuration: 4, animations: {
-        self.bird.center = CGPoint(x: self.view.bounds.size.width, y: self.view.bounds.size.height)
-    
-    
+        self.bird.center = CGPoint(x: self.view.bounds.size.width - 50, y: self.view.bounds.size.height - 50)
     
     }) { (finished) in
     
+    self.bird.transform = self.bird.transform.scaledBy(x: -1, y: 1).concatenating(CGAffineTransform(rotationAngle: 45))
+
+        UIView.animate(withDuration: 4, animations: {
+            self.bird.center = CGPoint(x: 0, y: 0)
+            
+        }) { (finished) in
+            
+            self.bird.transform = CGAffineTransform.identity
+            self.flyUpAndDown()
+            
+        
+        }
+    }
+}
+
+
+    func playSong() {
+    
+    let filepath = Bundle.main.path(forResource: "A+ – Chào Mào Mái Hót", ofType: ".mp3")
+    let url = URL(fileURLWithPath: filepath!)
+    audioPlayer = try! AVAudioPlayer(contentsOf: url)
+    audioPlayer.prepareToPlay()
+    audioPlayer.play()
     
 
 }
 
+override func viewWillDisappear(_ animated: Bool) {
+    audioPlayer.stop()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
